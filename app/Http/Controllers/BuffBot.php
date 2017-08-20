bbbedd27af3ac91bffd255284be86b4d03b3cd80
piyachetk <piyachetk@gmail.com>
2017-08-21 01:56:27 +0700

    - Added HTTP Auth

--- app/Http/Controllers/BuffBot.php
+++ app/Http/Controllers/BuffBot.php
@@ -19,6 +19,7 @@ class BuffBot extends Controller
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
         $response = curl_exec($curl);
         curl_close($curl);
         Log::info(var_export($response, true));
