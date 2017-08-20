e7da89d383fd2aedb1be37c450d746a4181238da
piyachetk <piyachetk@gmail.com>
2017-08-20 21:48:09 +0700

    - Updated response system

--- app/Http/Controllers/BuffBot.php
+++ app/Http/Controllers/BuffBot.php
@@ -7,6 +7,11 @@ use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use \LINE\LINEBot\Constant\HTTPHeader;
+use \LINE\LINEBot\Event\MessageEvent;
+use \LINE\LINEBot\Event\MessageEvent\TextMessage;
+use \LINE\LINEBot\Exception\InvalidEventRequestException;
+use \LINE\LINEBot\Exception\InvalidSignatureException;
+use Illuminate\Support\Facades\Log;
 
 class BuffBot extends Controller
 {
@@ -16,24 +21,31 @@ class BuffBot extends Controller
             abort(400);
         }
 
-        $jsonString = file_get_contents('php://input');
-        $jsonObject = json_decode($jsonString);
-
         $httpClient = new CurlHTTPClient('4nACR+eddtK+qgy7r8jX779s9vwyDsa3NKttV/ZyJS4UScLBklTJ67Fn+hA+K9gkEIxftlID070cOZKVF7xDgzA1CamEXAA/AhsA0sKhJz4OWpyn8FhJFYI9RDvsaml1rd41mu0r1HvYSAoPg+wEPQdB04t89/1O/w1cDnyilFU=');
         $bot = new LINEBot($httpClient, ['channelSecret' => '0d1623f60f2a8679726cf5d032564d11 ']);
 
-        $inputText = $jsonObject['events'][0]['message']['text'];
-        $replyToken = $jsonObject['events'][0]['replyToken'];
-
-        $textMessageBuilder = new TextMessageBuilder($inputText);
-        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-
-        if ($response->isSucceeded()) {
-            echo 'Succeeded!';
-            return;
+        try {
+            $events = $bot->parseEventRequest((object)$request->json()->all(), $signature[0]);
+        } catch (InvalidSignatureException $e) {
+            return abort(400);
+        } catch (InvalidEventRequestException $e) {
+            return abort(400);
         }
 
-        // Failed
-        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+        foreach ($events as $event) {
+            if (!($event instanceof MessageEvent)) {
+                Log::info('Non message event has come');
+                continue;
+            }
+            if (!($event instanceof TextMessage)) {
+                Log::info('Non text message has come');
+                continue;
+            }
+            $replyText = $event->getText();
+            Log::info('Reply text: ' . $replyText);
+            $resp = $bot->replyText($event->getReplyToken(), $replyText);
+            Log::info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
+        }
+        return response('OK', 200);
     }
 }
