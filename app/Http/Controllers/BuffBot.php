cd2bd9b239644a4d350455e70c3d4c23809d15dd
piyachetk <piyachetk@gmail.com>
2017-08-21 08:58:28 +0700

    - POST with array

--- app/Http/Controllers/BuffBot.php
+++ app/Http/Controllers/BuffBot.php
@@ -17,7 +17,7 @@ class BuffBot extends Controller
     {
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
         $response = curl_exec($curl);
